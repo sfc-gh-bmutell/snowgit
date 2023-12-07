33506ab2b5a949ebe1d737ea4f29821c6261b263
Bryan Mutell <48530379+sfc-gh-bmutell@users.noreply.github.com>
2023-12-06 17:27:01 -0700

    Update filter.py

--- filter.py
+++ filter.py
@@ -2,4 +2,4 @@ from snowflake.snowpark.functions import col
 
 def filter_by_role(session, table_name, user_name):
   df = session.table(table_name)
-  return df.filter(col("USER_NAME") == user_name)
+  return df.filter(col("role") == role)
